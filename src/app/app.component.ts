2a42e37bcd75679c8f92c4ef2370fad19bba07ab
Mark Griffiths <markg1704@hotmail.com>
2023-12-11 21:19:30 +0000

    navbar skeleton added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'evaluator-ui';
+
+  public isUser(): boolean {
+    // TODO - logic here
+    return false;
+  }
+
+  public onSignIn(): void {
+        // TODO - logic here
+    console.log("Sign in clicked")
+  }
+
+  public onSignOut(): void {
+        // TODO - logic here
+    console.log("Sign out Clicked");
+  }
 }
